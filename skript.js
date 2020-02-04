d42408140cdba3df7d65d48f11cd9d6b560fc5de
Albin H <57447840+Albin-H@users.noreply.github.com>
2020-02-04 09:25:26 +0100

    JavaScript
    Nu finns det en javascript som ändrar frågan när man trycker på nästa.

--- skript.js
+++ skript.js
@@ -1,7 +1,41 @@
-let counter = 0;
-
-function virus() {
-	counter++;
-	document.querySelector("#counter").innerHTML = counter;
-	window.open("https://www.youtube.com/");
+let nummer = 0;
+function svar() {
+	
+	
 } 
+
+const frågor = [
+	{
+		Fråga: "Vilket är världens minsta land ?"
+	},
+	{
+		Fråga: "Vilket land gränsar inte till Polen?"
+	},
+	{
+		Fråga: "Vad är Monsterrat?"
+	},
+	{
+		Fråga: "Vart är du om du bor i Kamtchakta"
+	},
+	{
+		Fråga: "Hur lång är den Kinesiska muren?"
+	},
+	{
+		Fråga: "Vilket språk talar man i Irkutsk"
+	},
+	{
+		Fråga: "Om Gizapyramiden ligger i Giza var ligger då Eifeltornet."
+	},
+	{
+		Fråga: "Var bor Påven"
+	},
+	{
+		Fråga: "Vilket är världens sämsta land"
+	},
+];
+
+function ladda() {
+	document.querySelector("#svar").innerHTML = "Fråga " + (nummer + 2);
+	document.querySelector("#fraga").innerHTML = frågor[nummer].Fråga;
+	nummer++;
+}
